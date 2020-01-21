d89d1a2d15a978f39a1593073f95d75e5937b83d
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2020-01-21 12:51:06 -0500

    Use termianl process id
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- src/utils/devCommands.ts
+++ src/utils/devCommands.ts
@@ -6,7 +6,7 @@ import * as Path from 'path';
 
 import { getReport } from './Util';
 
-export const terminals: { [libProjectId: string]: LibertyProject } = {};
+export const terminals: { [libProjectId: number]: LibertyProject } = {};
 
 // opens pom associated with LibertyProject and starts dev mode
 export async function openProject(pomPath: string): Promise<void> {
@@ -21,7 +21,9 @@ export async function startDevMode(libProject?: LibertyProject | undefined): Pro
         var terminal = libProject.getTerminal();
         if (terminal === undefined) {
             terminal = libProject.createTerminal();
-            terminals[libProject.getLabel() + " (liberty:dev)"] = libProject;
+            if (terminal !== undefined) {
+                terminals[Number(terminal.processId)] = libProject;
+            }
         }
         if (terminal !== undefined) {
             terminal.show();
@@ -61,7 +63,9 @@ export async function customDevMode(libProject?: LibertyProject | undefined): Pr
         var terminal = libProject.getTerminal();
         if (terminal === undefined) {
             terminal = libProject.createTerminal();
-            terminals[libProject.getLabel() + " (liberty:dev)"] = libProject;
+            if (terminal !== undefined) {
+                terminals[Number(terminal.processId)] = libProject;
+            }
         }
         if (terminal !== undefined) {
             terminal.show();
@@ -159,7 +163,7 @@ export async function openReport(reportType: string, libProject?: LibertyProject
 // retrieve LibertyProject correpsonding to closed terminal and delete terminal
 export function deleteTerminal(terminal: vscode.Terminal): void {
     try {
-        let libProject = terminals[terminal.name];
+        let libProject = terminals[Number(terminal.processId)];
         libProject.deleteTerminal();
     } catch {
         console.error("Unable to delete terminal: " + terminal.name);
