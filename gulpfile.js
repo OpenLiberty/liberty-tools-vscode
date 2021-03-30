fe09677dc1f27c7c0bc045a7e62e69d6a3bb578f
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2021-03-30 17:24:53 -0400

    build liberty-ls project with gulpfile
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 const gulp = require("gulp");
 const cp = require("child_process");
 
-const libertyLSName = "lemminx-liberty-1.0-SNAPSHOT-jar-with-dependencies.jar";
-const libertyLSDir = "../liberty-language-server/lemminx-liberty";
+const libertyLemminxName = "lemminx-liberty-1.0-SNAPSHOT-jar-with-dependencies.jar";
+const libertyLemminxDir = "../liberty-language-server/lemminx-liberty";
+const libertyLSName = "liberty.ls-1.0-SNAPSHOT-jar-with-dependencies.jar";
+const libertyLSDir = "../liberty-language-server/liberty-ls"
 
-gulp.task("buildServer", (done) => {
+gulp.task("buildLemminxLiberty", (done) => {
+  cp.execSync(mvnw() + " clean install -DskipTests", {
+    cwd: libertyLemminxDir,
+    stdio: "inherit",
+  });
+  gulp.src(libertyLemminxDir + "/target/" + libertyLemminxName).pipe(gulp.dest("./jars"));
+  done();
+});
+
+gulp.task("buildLibertyServer", (done) => {
   cp.execSync(mvnw() + " clean install -DskipTests", {
     cwd: libertyLSDir,
     stdio: "inherit",
@@ -12,6 +23,7 @@ gulp.task("buildServer", (done) => {
   gulp.src(libertyLSDir + "/target/" + libertyLSName).pipe(gulp.dest("./jars"));
   done();
 });
+
 
 function mvnw() {
   return isWin() ? "mvnw.cmd" : "./mvnw";
