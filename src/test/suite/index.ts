1bae452ab02623e44e5cc287832602e66b6f26e7
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2022-09-16 11:23:21 -0400

    Add copyright headers
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2020 IBM Corporation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 import * as glob from "glob";
 import * as Mocha from "mocha";
 import * as path from "path";
