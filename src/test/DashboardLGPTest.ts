f8a1b710854e574a574bf308778e755a41728890
Aparna Michael <Aparna.Michael@ibm.com>
2023-09-04 20:33:40 +0530

    Added test case for plugins block method

--- src/test/DashboardLGPTest.ts
+++ src/test/DashboardLGPTest.ts
@@ -17,6 +17,8 @@ describe('Devmode action tests for Gradle Project', () => {
   ** detect project with LGP config in build file only (ie. no server.xml in project)  **        
   *************************************************************************************/
 
+  //buildscript + apply plugin method
+
   it('Delete server.xml from config.', async () => {
 
     const dest = path.join(utils.getGradleProjectPath(), "src", "main", "liberty", "config", "server.xml");
@@ -44,6 +46,49 @@ describe('Devmode action tests for Gradle Project', () => {
 
   }).timeout(300000);
 
+
+
+  // plugins block method
+   it('Copy gradle.build file which uses plugins block method.', async () => {
+
+    await utils.delay(50000);
+    const serverxmlPath = path.join(utils.getGradleProjectPath(), "gradlefile", "build1.gradle");
+    const dest = path.join(utils.getGradleProjectPath(), "build.gradle");
+    await utils.copyFile(serverxmlPath, dest);
+    console.log("Finished copying file....");
+
+  }).timeout(90000);
+
+  it('getViewControl works with the correct label', async () => {
+
+    const contentPart = sidebar.getContent();
+    section = await contentPart.getSection('Liberty Dashboard');
+    console.log("Found Liberty Dashboard....");
+    expect(section).not.undefined;
+
+  }).timeout(10000);
+
+  it('Open dasboard shows items - Gradle', async () => {
+
+    await utils.delay(80000);
+    const menu = await section.getVisibleItems();
+    expect(menu).not.empty;
+    item = await section.findItem(constants.GRADLE_PROJECT) as DefaultTreeItem;
+    expect(item).not.undefined;
+
+  }).timeout(300000);
+
+  it('Copy gradle.build file which uses buildscript + apply plugin method.', async () => {
+
+    await utils.delay(50000);
+    const serverxmlPath = path.join(utils.getGradleProjectPath(), "gradlefile", "build2.gradle");
+    const dest = path.join(utils.getGradleProjectPath(), "build.gradle");
+    await utils.copyFile(serverxmlPath, dest);
+    console.log("Finished copying file....");
+
+  }).timeout(90000);
+
+  // Place server.xml back to the config folder.
   it('Copy server.xml to config folder', async () => {
 
     await utils.delay(50000);
@@ -53,6 +98,5 @@ describe('Devmode action tests for Gradle Project', () => {
     console.log("Finished copying file....");
 
   }).timeout(90000);
-
 });
 
