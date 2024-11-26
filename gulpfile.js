442d228d5100a87f250c5156d9748e51f11dd51b
Trevor Crawford <tcrawfo6@uwo.ca>
2024-11-26 16:36:02 -0500

    Update LSP4Jakarta to 0.2.2

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const cp = require("child_process");
 const libertyGroupId = "io.openliberty.tools";
 const libertyVersion = "2.2";
 const jakartaGroupId = "org.eclipse.lsp4jakarta";
-const jakartaVersion = "0.2.1";
+const jakartaVersion = "0.2.2";
 var releaseLevel = "releases";  //"snapshots"; //snapshots or releases
 
 const libertyLemminxName = "liberty-langserver-lemminx-" + libertyVersion + "-jar-with-dependencies.jar";
