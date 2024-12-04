79d86903bbbfc5afa337b6f7fa32e27b0d0217f2
SuparnaSuresh <suparna.suresh@ibm.com>
2024-12-04 19:42:38 +0530

    undo the format changes which are not part of the issue

--- src/test/utils/testUtils.ts
+++ src/test/utils/testUtils.ts
@@ -1,169 +1,171 @@
 import path = require('path');
 import { Workbench, InputBox, DefaultTreeItem, ModalDialog } from 'vscode-extension-tester';
 import * as fs from 'fs';
-import { MAVEN_PROJECT, STOP_DASHBOARD_MAC_ACTION } from '../definitions/constants';
+import { MAVEN_PROJECT, STOP_DASHBOARD_MAC_ACTION  } from '../definitions/constants';
 import { MapContextMenuforMac } from './macUtils';
 import clipboard = require('clipboardy');
 import { expect } from 'chai';
 import * as fse from 'fs-extra';
 
 export function delay(millisec: number) {
-  return new Promise(resolve => setTimeout(resolve, millisec));
+    return new Promise( resolve => setTimeout(resolve, millisec) );
 }
 
 export function getMvnProjectPath(): string {
-  const mvnProjectPath = path.join(__dirname, "..", "..", "..", "src", "test", "resources", "maven", "liberty.maven.test.wrapper.app");
-  console.log("Path is : " + mvnProjectPath);
-  return mvnProjectPath;
-}
+    const mvnProjectPath = path.join(__dirname, "..","..","..","src", "test","resources", "maven","liberty.maven.test.wrapper.app");  
+    console.log("Path is : "+mvnProjectPath)  ;
+    return mvnProjectPath; 
+  }
 
-export function getGradleProjectPath(): string {
-  const gradleProjectPath = path.join(__dirname, "..", "..", "..", "src", "test", "resources", "gradle", "liberty.gradle.test.wrapper.app");
-  console.log("Path is : " + gradleProjectPath);
-  return gradleProjectPath;
-}
+  export function getGradleProjectPath(): string {
+    const gradleProjectPath = path.join(__dirname, "..","..","..","src", "test","resources","gradle", "liberty.gradle.test.wrapper.app");  
+    console.log("Path is : "+gradleProjectPath)  ;
+    return gradleProjectPath; 
+  }
 
+ 
+ 
+  export async function launchDashboardAction(item: DefaultTreeItem, action: string, actionMac: string) {
 
-
-export async function launchDashboardAction(item: DefaultTreeItem, action: string, actionMac: string) {
-
-  console.log("Launching action:" + action);
-  if (process.platform === 'darwin') {//Only for MAC platform      
+    console.log("Launching action:" + action);  
+    if (process.platform === 'darwin') {//Only for MAC platform      
       await MapContextMenuforMac( item,actionMac);            
-  } else {  // NON MAC platforms    
+    } else {  // NON MAC platforms    
       console.log("before contextmenu")  ;
-    const menuItem = await item.openContextMenu();
+      const menuItem = await item.openContextMenu(); 
       console.log("before select")  ; 
-    await menuItem.select(action);
+      await menuItem.select(action);
+    }
+
+  }
+ 
+
+  export async function setCustomParameter(customParam: string) {
+
+    console.log("Setting custom Parameter");    
+    const input = new InputBox(); 
+    await input.click();   
+    await input.setText(customParam);      
+    await input.confirm();       
+  
   }
 
-}
+  export async function chooseCmdFromHistory(command: string): Promise<Boolean> {
 
-
-export async function setCustomParameter(customParam: string) {
-
-  console.log("Setting custom Parameter");
-  const input = new InputBox();
-  await input.click();
-  await input.setText(customParam);
-  await input.confirm();
-
-}
-
-export async function chooseCmdFromHistory(command: string): Promise<Boolean> {
-
-  console.log("Choosing command from history");
-  const input = new InputBox();
-  const pick = await input.findQuickPick(command);
+    console.log("Choosing command from history");    
+    const input = new InputBox();     
+    const pick = await input.findQuickPick(command);    
     if (pick){
-    await pick.select();
-    await input.confirm();
-    return true;
+      await pick.select();    
+      await input.confirm();       
+      return true;
+    }
+    else     
+      return false; 
   }
-  else
-    return false;
-}
 
   export async function deleteReports(reportPath:  string) : Promise<Boolean> {
 
-  //const reportPath = path.join(getMvnProjectPath(),"target","site","failsafe-report.html");
+    //const reportPath = path.join(getMvnProjectPath(),"target","site","failsafe-report.html");
     if (fs.existsSync(reportPath) )
     {
-    fs.unlink(reportPath, (err) => {
-      if (err)
-        return false;
+      fs.unlink(reportPath, (err) => {
+        if (err) 
+        return false; 
         else{
         console.log(reportPath+ ' was deleted');
         return true;
-      }
-    });
-  }
-  return true;
-}
+        }
+        });               
+    }    
+      return true;   
+    }
 
   export async function checkIfTestReportExists(reportPath: string) : Promise<Boolean> {
-  const maxAttempts = 10;
-  let foundReport = false;
-  //const reportPath = path.join(getMvnProjectPath(),"target","site","failsafe-report.html");
-  for (let i = 0; i < maxAttempts; i++) {
-    try {
-
+    const maxAttempts = 10;
+    let foundReport = false;
+    //const reportPath = path.join(getMvnProjectPath(),"target","site","failsafe-report.html");
+    for (let i = 0; i < maxAttempts; i++) {
+      try {
+                
           if (fs.existsSync(reportPath)) 
           {
-        foundReport = true;
-        break;
-      }
+            foundReport = true;
+            break;
+          }
           else{
-        await delay(5000);
-        foundReport = false;
-        continue;
+            await delay(5000);
+            foundReport = false;
+            continue;
+          }      
       }
-    }
       catch(e)
       {
-      console.error("Caught exception when checking for test report", e);
+        console.error("Caught exception when checking for test report", e);
 
+      }
     }
-  }
-  return foundReport;
-}
-
-
-
+    return foundReport;
+    }
+  
+    
+  
   export async function checkTerminalforServerState(serverStatusCode : string ): Promise <Boolean> {
-  const workbench = new Workbench();
+    const workbench = new Workbench();
     let  foundText = false;
     let count=0;    
     do{
-    clipboard.writeSync('');//clean slate for clipboard      
-    await workbench.executeCommand('terminal select all');
-    const text = clipboard.readSync();
+      clipboard.writeSync('');//clean slate for clipboard      
+      await workbench.executeCommand('terminal select all');       
+      const text = clipboard.readSync();        
       console.log("debug:" + text)      ;
       if( text.includes(serverStatusCode)){
-      foundText = true;
+        foundText = true;
         console.log("Found text "+ serverStatusCode);
-      break;
-    }
+        break;
+      }
       else if(text.includes("FAILURE"))
       {     
         console.log("Found failure "+ text);
-      foundText = false;
-      break;
-    }
+        foundText = false;             
+        break;      
+      }
       else
       {
-      console.log("test is running ...")
-      foundText = false;
-    }
-    count++;
-    await workbench.getDriver().sleep(10000);
+        console.log("test is running ...")
+        foundText = false;
+      }
+      count++;   
+      await workbench.getDriver().sleep(10000);
     } while(!foundText && (count <= 20));
-  await workbench.executeCommand('terminal clear');
-  return foundText;
-}
-
+    await workbench.executeCommand('terminal clear');
+    return foundText;
+  }
+  
 
   export async function checkTestStatus(testStatus: string): Promise <Boolean>{
-  const workbench = new Workbench();
+    const workbench = new Workbench();
     let  foundText = false;
     let count=0;    
     do{
-    clipboard.writeSync('');
-    await workbench.executeCommand('terminal select all');
-    const text = clipboard.readSync();
+      clipboard.writeSync('');
+      await workbench.executeCommand('terminal select all');      
+      const text = clipboard.readSync();         
       if( text.includes(testStatus)){
-      foundText = true;
+        foundText = true;
         console.log("Found text "+ testStatus);
-      break;
-    }
-    else
-      foundText = false;
-    count++;
-    await workbench.getDriver().sleep(2000);
+        break;
+      }      
+      else
+        foundText = false;
+      count++;   
+      await workbench.getDriver().sleep(2000);
     } while(!foundText && (count <= 5));
-  await workbench.executeCommand('terminal clear');
-  return foundText;
-}
+    await workbench.executeCommand('terminal clear');
+    return foundText;
+  }
+
+  
 
 /* Stop Server Liberty dashboard post Attach Debugger*/
 /* As the Window view changes using command to stop server instead of devmode action */
@@ -181,17 +183,16 @@ export async function stopLibertyserver() {
 
 export async function clearCommandPalette() {
   await new Workbench().executeCommand('Clear Command History');
-  await delay(30000);
+  await delay(30000);  
   const dialog = new ModalDialog();
   const message = await dialog.getMessage();
 
   expect(message).contains('Do you want to clear the history of recently used commands?');
-
+  
   const buttons =  await dialog.getButtons();
   expect(buttons.length).equals(2);
   await dialog.pushButton('Clear');
 }
-
 /**
  * Function return project name with space
  */
