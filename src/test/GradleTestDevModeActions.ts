8e8a5caa0ce6a8f8072f0ef815b13fe3bb4d36f9
SuparnaSuresh <suparna.suresh@ibm.com>
2025-01-14 22:05:55 +0530

    Reverted the aligment related changes for the code

--- src/test/GradleTestDevModeActions.ts
+++ src/test/GradleTestDevModeActions.ts
@@ -6,234 +6,234 @@ import path = require('path');
 import { viewTestReportForMac } from './utils/macUtils';
 
 describe('Devmode action tests for Gradle Project', () => {
-  let sidebar: SideBarView;
-  let debugView: DebugView;
-  let section: ViewSection;
-  let item: DefaultTreeItem;
-  let tabs: string[];
+    let sidebar: SideBarView;
+    let debugView: DebugView;
+    let section: ViewSection;
+    let item: DefaultTreeItem;    
+    let tabs: string[];
 
-  before(() => {
-    sidebar = new SideBarView();
-    debugView = new DebugView();
-  });
+    before(() => {
+        sidebar = new SideBarView();
+        debugView = new DebugView();      
+    });
 
 it('getViewControl works with the correct label',  async() => { 
+   
+   const contentPart = sidebar.getContent();
+   section = await contentPart.getSection('Liberty Dashboard');   
+   console.log("Found Liberty Dashboard....");
+   expect(section).not.undefined; 
+ 
+}).timeout(10000);
 
-    const contentPart = sidebar.getContent();
-    section = await contentPart.getSection('Liberty Dashboard');
-    console.log("Found Liberty Dashboard....");
-    expect(section).not.undefined;
 
-  }).timeout(10000);
-
-
-  it('Open dasboard shows items - Gradle', async () => {
+it('Open dasboard shows items - Gradle', async () => {
 
     // Wait for the Liberty Dashboard to load and expand. The dashboard only expands after using the 'expand()' method.  
     await utils.delay(65000);
-    await section.expand();
+    await section.expand(); 
     await utils.delay(6000);
-    const menu = await section.getVisibleItems();
-    expect(menu).not.empty;
-    item = await section.findItem(constants.GRADLE_PROJECT) as DefaultTreeItem;
-    expect(item).not.undefined;
+    const menu = await section.getVisibleItems();            
+    expect(menu).not.empty;     
+    item = await section.findItem(constants.GRADLE_PROJECT) as DefaultTreeItem;   
+    expect(item).not.undefined;   
+   
+    
+}).timeout(300000);
 
 
-  }).timeout(300000);
-
-
-  it('Start gradle project from liberty dashboard', async () => {
-
-
+it('Start gradle project from liberty dashboard', async () => {      
+    
+  
   await utils.launchDashboardAction(item,constants.START_DASHBOARD_ACTION,constants.START_DASHBOARD_MAC_ACTION);  
-    await utils.delay(30000);
-    const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
+  await utils.delay(30000);
+  const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
   if(!serverStartStatus)
-      console.log("Server started message not found in the terminal");
+    console.log("Server started message not found in the terminal");
   else
   {
-      console.log("Server succuessfully started");
-      await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);
+    console.log("Server succuessfully started");  
+    await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);
     const serverStopStatus= await utils.checkTerminalforServerState(constants.SERVER_STOP_STRING);   
     if(!serverStopStatus){ 
-        console.error("Server stopped message not found in the terminal");
-      }
-      else
-        console.log("Server stopped successfully");
-    expect (serverStopStatus).to.be.true;
+    console.error("Server stopped message not found in the terminal");
     }
+    else
+      console.log("Server stopped successfully");
+    expect (serverStopStatus).to.be.true;
+}
  expect (serverStartStatus).to.be.true; 
+ 
+    
+}).timeout(350000);
 
-
-  }).timeout(350000);
-
-  it('Run tests for gradle project', async () => {
-
-    await utils.launchDashboardAction(item, constants.START_DASHBOARD_ACTION, constants.START_DASHBOARD_MAC_ACTION);
-    await utils.delay(30000);
-    const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
-    console.log("after checkTerminalforServerState");
+it('Run tests for gradle project', async () => {  
+  
+  await utils.launchDashboardAction(item, constants.START_DASHBOARD_ACTION, constants.START_DASHBOARD_MAC_ACTION);
+  await utils.delay(30000);  
+  const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
+  console.log("after checkTerminalforServerState"); 
   if(!serverStartStatus)
-      console.log("Server started message not found in the terminal");
+    console.log("Server started message not found in the terminal");
   else
   {
-      console.log("Server succuessfully started");
+    console.log("Server succuessfully started");  
     await utils.launchDashboardAction(item,constants.RUNTEST_DASHBOARD_ACTION,constants.RUNTEST_DASHBOARD_MAC_ACTION);    
-      const testStatus = await utils.checkTestStatus(constants.GRADLE_TEST_RUN_STRING);
-      await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);
+    const testStatus = await utils.checkTestStatus(constants.GRADLE_TEST_RUN_STRING);        
+    await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);
     const serverStopStatus= await utils.checkTerminalforServerState(constants.SERVER_STOP_STRING);
     if(!serverStopStatus)
-        console.error("Server stopped message not found in the terminal");
-      else
-        console.log("Server stopped successfully");
+      console.error("Server stopped message not found in the terminal");    
+    else
+      console.log("Server stopped successfully");
     expect (serverStopStatus).to.be.true;
     expect (testStatus).to.be.true;
-    }
+}
  expect (serverStartStatus).to.be.true; 
+ 
+    
+}).timeout(350000);
 
 
-  }).timeout(350000);
-
-
-  it('start gradle with options from liberty dashboard', async () => {
-
+it('start gradle with options from liberty dashboard', async () => {      
+    
   const reportPath = path.join(utils.getGradleProjectPath(),"build", "reports", "tests", "test", "index.html");
-    const deleteReport = await utils.deleteReports(reportPath);
+  const deleteReport = await utils.deleteReports(reportPath);
   expect (deleteReport).to.be.true;
-    await utils.launchDashboardAction(item, constants.START_DASHBOARD_ACTION_WITH_PARAM, constants.START_DASHBOARD_MAC_ACTION_WITH_PARAM);
-    await utils.setCustomParameter("--hotTests");
-    await utils.delay(30000);
-    const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
+  await utils.launchDashboardAction(item, constants.START_DASHBOARD_ACTION_WITH_PARAM, constants.START_DASHBOARD_MAC_ACTION_WITH_PARAM);
+  await utils.setCustomParameter("--hotTests");  
+  await utils.delay(30000);  
+  const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
   if(!serverStartStatus)
-      console.log("Server started with params message not found in terminal ");
+    console.log("Server started with params message not found in terminal ");
   else
   {
-      console.log("Server succuessfully started");
-      let checkFile = await utils.checkIfTestReportExists(reportPath);
-      await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);
-      console.log("after dashboard action");
+    console.log("Server succuessfully started");  
+    let checkFile = await utils.checkIfTestReportExists(reportPath);    
+    await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);    
+    console.log("after dashboard action");
     const serverStopStatus= await utils.checkTerminalforServerState(constants.SERVER_STOP_STRING);
     if(!serverStopStatus) 
-        console.error("Server stopped message not found in ther terminal");
-      else
-        console.log("Server stopped successfully");
+      console.error("Server stopped message not found in ther terminal");    
+    else
+      console.log("Server stopped successfully");
     expect (serverStopStatus).to.be.true;
     expect (checkFile).to.be.true;    
-    }
+}
  expect (serverStartStatus).to.be.true;
+    
+}).timeout(550000);
 
-  }).timeout(550000);
-
-  it('start gradle with history from liberty dashboard', async () => {
+it('start gradle with history from liberty dashboard', async () => {  
 
   const reportPath = path.join(utils.getGradleProjectPath(),"build", "reports", "tests", "test", "index.html");
-    const deleteReport = await utils.deleteReports(reportPath);
+  const deleteReport = await utils.deleteReports(reportPath);
   expect (deleteReport).to.be.true;  
-    await utils.launchDashboardAction(item, constants.START_DASHBOARD_ACTION_WITH_PARAM, constants.START_DASHBOARD_MAC_ACTION_WITH_PARAM);
-    const foundCommand = await utils.chooseCmdFromHistory("--hotTests");
-    console.log("foundcmd:" + foundCommand);
+  await utils.launchDashboardAction(item, constants.START_DASHBOARD_ACTION_WITH_PARAM, constants.START_DASHBOARD_MAC_ACTION_WITH_PARAM);  
+  const foundCommand = await utils.chooseCmdFromHistory("--hotTests");
+  console.log("foundcmd:" + foundCommand);
   expect (foundCommand).to.be.true;  
-    await utils.delay(30000);
-    const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
+  await utils.delay(30000);
+  const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
   if(!serverStartStatus)
-      console.log("Server started with params message not found in the terminal ");
+    console.log("Server started with params message not found in the terminal ");
   else
   {
-      console.log("Server succuessfully started");
-      let checkFile = await utils.checkIfTestReportExists(reportPath);
-      await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);
+    console.log("Server succuessfully started");  
+    let checkFile = await utils.checkIfTestReportExists(reportPath);    
+    await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);    
     const serverStopStatus= await utils.checkTerminalforServerState(constants.SERVER_STOP_STRING);
     if(!serverStopStatus)
-        console.error("Server stopped message not found in terminal");
-      else
-        console.log("Server stopped successfully");
+      console.error("Server stopped message not found in terminal");
+    else
+      console.log("Server stopped successfully");
     expect (serverStopStatus).to.be.true;
     expect (checkFile).to.be.true;
-    }
+}
  expect (serverStartStatus).to.be.true;
+    
+}).timeout(350000);
 
-  }).timeout(350000);
 
-
-  it('attach debugger for gradle with custom parameter event', async () => {
-    console.log("start attach debugger");
-    let isServerRunning: Boolean = true;
-    let attachStatus: Boolean = false;
-    try {
+it('attach debugger for gradle with custom parameter event', async () => {
+  console.log("start attach debugger");
+  let isServerRunning: Boolean = true;
+  let attachStatus: Boolean = false;
+  try {
     await utils.launchDashboardAction(item,constants.START_DASHBOARD_ACTION_WITH_PARAM, constants.START_DASHBOARD_MAC_ACTION_WITH_PARAM);
-      await utils.setCustomParameter("-DdebugPort=7777");
-      await utils.delay(30000);
-
-      isServerRunning = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
-      if (!isServerRunning)
-        console.log("Server started with params message not found in terminal");
-      else {
-        console.log("Server succuessfully started");
+    await utils.setCustomParameter("-DdebugPort=7777");   
+    await utils.delay(30000);
+    
+    isServerRunning = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
+    if (!isServerRunning)
+      console.log("Server started with params message not found in terminal");
+    else {
+      console.log("Server succuessfully started");
     await utils.launchDashboardAction(item,constants.ATTACH_DEBUGGER_DASHBOARD_ACTION, constants.ATTACH_DEBUGGER_DASHBOARD_MAC_ACTION);    
-        console.log("Attach Debugger action done");
-        await utils.delay(8000);
-        const contentPart = debugView.getContent();
-        //console.log("Get Content");
-
-        let mysecarry: Promise<ViewSection[]> = contentPart.getSections();
-        let mysecmap: IterableIterator<[number, ViewSection]> = (await mysecarry).entries();
-        for (const [key, value] of (mysecmap)) {
-          if ((await value.getEnclosingElement().getText()).includes("BREAKPOINTS")) {
-            //console.log("******** mysecmap getEnclosingElement " + (await value.getEnclosingElement().getText()).valueOf());
-            console.log("Found Breakpoints");
-            attachStatus = true;
-            break;
-          }
-        }
-
-        await utils.stopLibertyserver();
-        isServerRunning = !await utils.checkTerminalforServerState(constants.SERVER_STOP_STRING); //negate isServerRunning
-        if (!isServerRunning)
-          console.log("Server stopped successfully ");
+    console.log("Attach Debugger action done");
+    await utils.delay(8000);
+    const contentPart = debugView.getContent();
+    //console.log("Get Content");
+    
+    let mysecarry: Promise<ViewSection[]> = contentPart.getSections();    
+    let mysecmap: IterableIterator<[number, ViewSection]> = (await mysecarry).entries();
+    for (const [key, value] of (mysecmap)) {
+      if ((await value.getEnclosingElement().getText()).includes("BREAKPOINTS")) {
+        //console.log("******** mysecmap getEnclosingElement " + (await value.getEnclosingElement().getText()).valueOf());
+        console.log("Found Breakpoints");
+        attachStatus = true;
+        break;
       }
-    } catch (e) {
-      console.error("error - ", e)
-    } finally {
-      console.log("finally block: is server running -  ", isServerRunning);
-      if (isServerRunning) {
-        utils.stopLibertyserver();
-      }
-      else
-        console.log("good to close test - Attach Debugger for start with custom parameter(-DdebugPort=7777) event");
     }
-    expect(attachStatus).to.be.true;
-  }).timeout(550000);
 
-  it('start gradle with docker from liberty dashboard', async () => {
+    await utils.stopLibertyserver();
+    isServerRunning = !await utils.checkTerminalforServerState(constants.SERVER_STOP_STRING); //negate isServerRunning
+    if (!isServerRunning)
+      console.log("Server stopped successfully ");
+  }
+  } catch (e) {
+    console.error("error - ", e)
+  } finally {
+    console.log("finally block: is server running -  ", isServerRunning);
+    if (isServerRunning) {
+      utils.stopLibertyserver();
+    }
+    else
+      console.log("good to close test - Attach Debugger for start with custom parameter(-DdebugPort=7777) event");
+  }
+  expect(attachStatus).to.be.true;
+}).timeout(550000);
 
+it('start gradle with docker from liberty dashboard', async () => {     
+  
   if((process.platform === 'darwin' ) || (process.platform === 'win32') || (process.platform == 'linux'))
   {
-      //skip running for platforms , enable them for linux after resolving docker setup in GHA
-      return true;
-    }
-
-
-    await utils.launchDashboardAction(item, constants.START_DASHBOARD_ACTION_WITHDOCKER, constants.START_DASHBOARD_MAC_ACTION_WITHDOCKER);
-    await utils.delay(60000);
-    const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
+    //skip running for platforms , enable them for linux after resolving docker setup in GHA
+    return true;
+  }
+    
+  
+  await utils.launchDashboardAction(item, constants.START_DASHBOARD_ACTION_WITHDOCKER, constants.START_DASHBOARD_MAC_ACTION_WITHDOCKER);  
+  await utils.delay(60000);
+  const serverStartStatus = await utils.checkTerminalforServerState(constants.SERVER_START_STRING);
   if(!serverStartStatus)
-      console.log("Server started message not found in the terminal");
+    console.log("Server started message not found in the terminal");
   else
   {
-      console.log("Server succuessfully started");
-      await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);
+    console.log("Server succuessfully started");  
+    await utils.launchDashboardAction(item, constants.STOP_DASHBOARD_ACTION, constants.STOP_DASHBOARD_MAC_ACTION);    
     const serverStopStatus= await utils.checkTerminalforServerState(constants.SERVER_STOP_STRING);
     if(!serverStopStatus){ 
-        console.error("Server stopped message not found in the terminal");
-      }
-      else
-        console.log("Server stopped successfully");
-    expect (serverStopStatus).to.be.true;
+    console.error("Server stopped message not found in the terminal");
     }
+    else
+      console.log("Server stopped successfully");
+    expect (serverStopStatus).to.be.true;
+}
  expect (serverStartStatus).to.be.true; 
-
-
-  }).timeout(350000);
+ 
+    
+}).timeout(350000);
 
   it('View test report for gradle project', async () => {
 
@@ -260,4 +260,3 @@ it('getViewControl works with the correct label',  async() => {
   }).timeout(100000);
 
 });
-
