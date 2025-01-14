689cc0bb6eb530175d351e855885b59cacdc5c74
gilbysunil14 <Gilby.Sunil@ibm.com>
2025-01-14 12:16:35 +0530

    revert testing changes

--- src/test/MavenTestDevModeActions.ts
+++ src/test/MavenTestDevModeActions.ts
@@ -240,7 +240,7 @@ it('attach debugger for start with custom parameter event', async () => {
 
  it('start maven with docker from liberty dashboard', async () => {      
 
-  if((process.platform === 'darwin' ) || (process.platform === 'win32'))
+  if((process.platform === 'darwin' ) || (process.platform === 'win32') || (process.platform == 'linux'))
   {
     //skip running for platforms , enable them for linux after resolving docker setup in GHA
     return true;
