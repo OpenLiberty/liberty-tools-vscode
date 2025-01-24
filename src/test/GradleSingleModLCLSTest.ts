746fbf36c35794a6f717a765baf41aaf2880f13f
SuparnaSuresh <suparna.suresh@ibm.com>
2025-01-25 00:40:49 +0530

    updated variable names and resolved typo

--- src/test/GradleSingleModLCLSTest.ts
+++ src/test/GradleSingleModLCLSTest.ts
@@ -36,28 +36,28 @@ describe('LCLS tests for Gradle Project', function () {
     it('Should show diagnostic for server.xml invalid value', async () => {
         await utils.openServerXMLFile();
 
-        const hoverExpectedOutcome = `'wrong' is not a valid value of union type 'booleanType'.`;
-        const testHverTarget = '<logging appsWriteJson = \"wrong\" />';
+        const expectedOutcomeValue = `'wrong' is not a valid value of union type 'booleanType'.`;
+        const hoverTargetValue = '<logging appsWriteJson = \"wrong\" />';
 
-        await editor.typeTextAt(17, 5, testHverTarget);
-        const focusTargtElemnt = editor.findElement(By.xpath("//*[contains(text(), 'wrong')]"));
+        await editor.typeTextAt(17, 5, hoverTargetValue);
+        const focusTargetedElement = editor.findElement(By.xpath("//*[contains(text(), 'wrong')]"));
         await utils.delay(3000);
-        focusTargtElemnt.click();
+        focusTargetedElement.click();
         await editor.click();
 
-        const actns = VSBrowser.instance.driver.actions();
-        await actns.move({ origin: focusTargtElemnt }).perform();
+        const actionList = VSBrowser.instance.driver.actions();
+        await actionList.move({ origin: focusTargetedElement }).perform();
         await utils.delay(5000);
 
-        const hoverContent = editor.findElement(By.className('hover-contents'));
-        const hoverFoundOutcome = await hoverContent.getText();
+        const hoverContents = editor.findElement(By.className('hover-contents'));
+        const hoverFoundOutcome = await hoverContents.getText();
         console.log("Hover text is:" + hoverFoundOutcome);
 
-        assert(hoverFoundOutcome.includes(hoverExpectedOutcome), 'Did not get expected diagnostic in server.xml');
+        assert(hoverFoundOutcome.includes(expectedOutcomeValue), 'Did not get expected diagnostic in server.xml');
 
         editor.clearText();
         editor.setText(actualServerXMLContent);
-        console.log("Content restored");
+        console.log("server.xml content restored");
 
     }).timeout(35000);
 
@@ -66,25 +66,25 @@ describe('LCLS tests for Gradle Project', function () {
 
         editor = await new EditorView().openEditor('server.xml') as TextEditor;
         const stanzaSnippet = "<logging appsWriteJson = \"wrong\" />";
-        const expectedHoverSnippet = "<logging appsWriteJson = \"true\" />";
+        const hoverExpectedSnippet = "<logging appsWriteJson = \"true\" />";
         await editor.typeTextAt(17, 5, stanzaSnippet);
         await utils.delay(2000);
-        const flaggedString = await editor.findElement(By.xpath("//*[contains(text(), '\"wrong\"')]"));
+        const hoverTargetValue = await editor.findElement(By.xpath("//*[contains(text(), '\"wrong\"')]"));
         await utils.delay(7000);
 
-        const actions = VSBrowser.instance.driver.actions();
-        await actions.move({ origin: flaggedString }).perform();
+        const actionList = VSBrowser.instance.driver.actions();
+        await actionList.move({ origin: hoverTargetValue }).perform();
         await utils.delay(3000);
 
         const driver = VSBrowser.instance.driver;
-        const hoverTxt = await editor.findElement(By.className('hover-row status-bar'));
+        const hoverRowStatusBar = await editor.findElement(By.className('hover-row status-bar'));
         await utils.delay(2000);
 
-        const qckFixPopupLink = await hoverTxt.findElement(By.xpath("//*[contains(text(), 'Quick Fix')]"));
-        await qckFixPopupLink.click();
+        const quickFixPopupLink = await hoverRowStatusBar.findElement(By.xpath("//*[contains(text(), 'Quick Fix')]"));
+        await quickFixPopupLink.click();
 
-        const hoverTaskBar = await editor.findElement(By.className('context-view monaco-component bottom left fixed'));
-        await hoverTaskBar.findElement(By.className('actionList'));
+        const hoverWindowTaskBar = await editor.findElement(By.className('context-view monaco-component bottom left fixed'));
+        await hoverWindowTaskBar.findElement(By.className('actionList'));
         await utils.delay(2000);
 
         const pointerBlockedElement = await driver.findElement(By.css('.context-view-pointerBlock'));
@@ -94,43 +94,43 @@ describe('LCLS tests for Gradle Project', function () {
         } else {
             console.log('pointerBlockElement is not found!');
         }
-        const quickfixOption = await editor.findElement(By.xpath("//*[contains(text(), \"Replace with 'true'\")]"));
-        await quickfixOption.click();
+        const quickfixOptionValues = await editor.findElement(By.xpath("//*[contains(text(), \"Replace with 'true'\")]"));
+        await quickfixOptionValues.click();
 
         const updatedSeverXMLContent = await editor.getText();
         await utils.delay(3000);
         console.log("Content after Quick fix : ", updatedSeverXMLContent);
-        assert(updatedSeverXMLContent.includes(expectedHoverSnippet), 'Quick fix not applied correctly for the invalid value in server.xml.');
+        assert(updatedSeverXMLContent.includes(hoverExpectedSnippet), 'Quick fix not applied correctly for the invalid value in server.xml.');
 
         editor.clearText();
         editor.setText(actualServerXMLContent);
-        console.log("Content restored");
+        console.log("server.xml content restored");
 
     }).timeout(38000);
 
     it('Should show hover support for server.xml Liberty Server Attribute', async () => {
         await utils.openServerXMLFile();
 
-        const hoverOutcome = `Configuration properties for an HTTP endpoint.`;
+        const hoverExpectedOutcome = `Configuration properties for an HTTP endpoint.`;
 
         const focusTargetedElement = editor.findElement(By.xpath("//*[contains(text(), 'httpEndpoint')]"));
         await utils.delay(3000);
         focusTargetedElement.click();
         await editor.click();
 
-        const actions = VSBrowser.instance.driver.actions();
-        await actions.move({ origin: focusTargetedElement }).perform();
+        const actionList = VSBrowser.instance.driver.actions();
+        await actionList.move({ origin: focusTargetedElement }).perform();
         await utils.delay(5000);
 
         const hoverContent = editor.findElement(By.className('hover-contents'));
         const hoveredTextValue = await hoverContent.getText();
         console.log("Hover text is: " + hoveredTextValue);
 
-        assert(hoveredTextValue.includes(hoverOutcome), 'Did not get expected hover data Liberty Server Attribute.');
+        assert(hoveredTextValue.includes(hoverExpectedOutcome), 'Did not get expected hover data Liberty Server Attribute.');
 
         editor.clearText();
         editor.setText(actualServerXMLContent);
-        console.log("Content is restored");
+        console.log("server.xml content restored");
 
     }).timeout(35000);
 
@@ -148,19 +148,19 @@ describe('LCLS tests for Gradle Project', function () {
         focusTargetElement.click();
         await editor.click();
 
-        const actions = VSBrowser.instance.driver.actions();
-        await actions.move({ origin: focusTargetElement }).perform();
+        const actionList = VSBrowser.instance.driver.actions();
+        await actionList.move({ origin: focusTargetElement }).perform();
         await utils.delay(5000);
 
-        const hoverContent = editor.findElement(By.className('hover-contents'));
-        const hoveredValue = await hoverContent.getText();
+        const hoverContents = editor.findElement(By.className('hover-contents'));
+        const hoveredValue = await hoverContents.getText();
         console.log("Hover text is :" + hoveredValue);
 
         assert(hoveredValue.includes(hoverOutcome), 'Did not get expected hover data Liberty Server Feature.');
 
         editor.clearText();
         editor.setText(actualServerXMLContent);
-        console.log("Content restored");
+        console.log("server.xml content restored");
 
     }).timeout(33000);
 
@@ -170,38 +170,37 @@ describe('LCLS tests for Gradle Project', function () {
         editor = await new EditorView().openEditor('server.xml') as TextEditor;
         const featureTag = "<f";
 
-        const addFeature = "<feature>el-3.0</feature>";
+        const addFeatureValue = "<feature>el-3.0</feature>";
         await editor.typeTextAt(15, 35, '\n');
         await editor.typeTextAt(16, 9, featureTag);
         await utils.delay(5000);
         //open the assistant
-        let asist = await editor.toggleContentAssist(true);
+        let assist = await editor.toggleContentAssist(true);
         // toggle can return void, so we need to make sure the object is present
-        if (asist) {
+        if (assist) {
             // to select an item use
-            await asist.select('feature')
+            await assist.select('feature')
         }
-        // close the assistant
-        await editor.toggleContentAssist(false);
-        const stanzaSnipet = "el-3";
 
-        await editor.typeTextAt(16, 18, stanzaSnipet);
+        const stanzaSnippet = "el-3";
+
+        await editor.typeTextAt(16, 18, stanzaSnippet);
         await utils.delay(5000);
 
-        asist = await editor.toggleContentAssist(true);
-        if (asist) {
-            await asist.select('el-3.0')
+        assist = await editor.toggleContentAssist(true);
+        if (assist) {
+            await assist.select('el-3.0')
         }
         await editor.toggleContentAssist(false);
 
         const updatedServerxmlContent = await editor.getText();
         await utils.delay(3000);
         console.log("Content after type ahead support : ", updatedServerxmlContent);
-        assert(updatedServerxmlContent.includes(addFeature), 'Type ahead support is not worked as expected in server.xml Liberty Server Feature - el-3.0.');
+        assert(updatedServerxmlContent.includes(addFeatureValue), 'Type ahead support is not worked as expected in server.xml Liberty Server Feature - el-3.0.');
 
         editor.clearText();
         editor.setText(actualServerXMLContent);
-        console.log("Content restored");
+        console.log("server.xml content restored");
 
     }).timeout(35000);
 
@@ -211,7 +210,7 @@ describe('LCLS tests for Gradle Project', function () {
         editor = await new EditorView().openEditor('server.xml') as TextEditor;
         const stanzaSnippet = "log";
 
-        const insertedConfig = "<logging></logging>";
+        const insertedConfigValue = "<logging></logging>";
         await editor.typeTextAt(17, 5, stanzaSnippet);
         await utils.delay(5000);
         //open the assistant
@@ -227,11 +226,11 @@ describe('LCLS tests for Gradle Project', function () {
         const updatedServerxmlContent = await editor.getText();
         await utils.delay(3000);
         console.log("Updated content in Sever.xml : ", updatedServerxmlContent);
-        assert(updatedServerxmlContent.includes(insertedConfig), 'Type ahead support is not worked as expected in server.xml Liberty Server Configuration Stanza');
+        assert(updatedServerxmlContent.includes(insertedConfigValue), 'Type ahead support is not worked as expected in server.xml Liberty Server Configuration Stanza');
 
         editor.clearText();
         editor.setText(actualServerXMLContent);
-        console.log("Content restored");
+        console.log("server.xml content restored");
 
     }).timeout(25000);
 
