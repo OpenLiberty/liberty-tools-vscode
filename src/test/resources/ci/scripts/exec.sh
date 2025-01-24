6d1e8d544fe2316e733f7d75ec72a82b33b53781
gilbysunil14 <Gilby.Sunil@ibm.com>
2025-01-24 11:18:14 +0530

    run test

--- src/test/resources/ci/scripts/exec.sh
+++ src/test/resources/ci/scripts/exec.sh
@@ -65,8 +65,7 @@ main() {
              if [ $OS = "Darwin" ]; then
                 chown -R runner src/test/resources/maven
               chown -R runner  src/test/resources/gradle
-                npm run test-mac-gradle -- -u
-                npm run test-mac-maven -- -u
+                npm run test -- -u
             else
                 npm run test -- -u
             fi
@@ -75,9 +74,7 @@ main() {
             if [ $OS = "Darwin" ]; then
               chown -R runner src/test/resources/maven
               chown -R runner  src/test/resources/gradle
-              npm run test-mac-gradle -- -u -c $VSCODE_VERSION_TO_RUN
-              rc=$?
-              npm run test-mac-maven -- -u -c $VSCODE_VERSION_TO_RUN
+              npm run test -- -u -c $VSCODE_VERSION_TO_RUN
 
             else
             npm run test -- -u -c $VSCODE_VERSION_TO_RUN
