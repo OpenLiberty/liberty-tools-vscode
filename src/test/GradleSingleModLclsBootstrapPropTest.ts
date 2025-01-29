85e712fce700a51d75572c495f9b7f5fae8a39a7
SuparnaSuresh <suparna.suresh@ibm.com>
2025-01-29 20:17:25 +0530

    test cases updated as a seperate file

---
+++ src/test/GradleSingleModLclsBootstrapPropTest.ts
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2025 IBM Corporation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+import { By, EditorView, TextEditor, VSBrowser } from "vscode-extension-tester";
+import * as utils from './utils/testUtils';
+import * as constants from './definitions/constants';
+
+const path = require('path');
+const assert = require('assert');
+
+describe('LCLS tests for Gradle Project - bootstrap.properties', function () {
+    let editor: TextEditor;
+
+    before(() => {
+        utils.copyDirectoryByPath(path.join(utils.getGradleProjectPath(), 'src', 'main', 'liberty', 'config'), path.join(utils.getGradleProjectPath(), 'src', 'main', 'liberty', 'config2'));
+    });
+
+    it('Should show diagnostic support in boostrap.properties ', async () => {
+        await utils.openConfigFile(constants.CONFIG_TWO, constants.BOOTSTRAP_PROPERTIES);
+        editor = await new EditorView().openEditor(constants.BOOTSTRAP_PROPERTIES) as TextEditor;
+
+        await editor.typeTextAt(1, 1, constants.WS_LOGGING_CON);
+        await utils.delay(5000);
+        //open the assistant
+        await utils.callAssitantAction(editor, constants.WS_LOGGING_CONSOLE_FORMAT);
+        // close the assistant
+        await editor.toggleContentAssist(false);
+
+        await editor.typeTextAt(1, 34, constants.VALUE_WRONG);
+        const focusTargetedElement = editor.findElement(By.xpath(constants.FOCUS_WRONG));
+        await utils.delay(3000);
+        focusTargetedElement.click();
+        await editor.click();
+
+        const actions = VSBrowser.instance.driver.actions();
+        await actions.move({ origin: focusTargetedElement }).perform();
+        await utils.delay(5000);
+
+        const hoverContents = editor.findElement(By.className('hover-contents'));
+        const hoverValue = await hoverContents.getText();
+        console.log("Expected text is:" + constants.WS_LOGGING_CONSOLE_DIAGNOSTIC);
+        console.log("Hover text is:" + hoverValue);
+
+        assert(hoverValue.includes(constants.WS_LOGGING_CONSOLE_DIAGNOSTIC), 'Did not get expected diagnostic as expected in boostrap.properties.');
+        editor.clearText();
+        await utils.closeEditor(constants.BOOTSTRAP_PROPERTIES);
+
+    }).timeout(55000);
+
+    it('Should show hover support for bootstrap.properties Liberty Server properties setting', async () => {
+        await utils.openConfigFile(constants.CONFIG_TWO, constants.BOOTSTRAP_PROPERTIES);
+        editor = await new EditorView().openEditor(constants.BOOTSTRAP_PROPERTIES) as TextEditor;
+
+        await editor.clearText();
+        await editor.typeTextAt(1, 1, constants.WS_LOGGING_CONSOLE_VALUE);
+        await utils.delay(5000);
+
+        const focusTargetLement = editor.findElement(By.xpath(constants.FOCUS_WS_LOGLEVEL));
+        await utils.delay(3000);
+        focusTargetLement.click();
+        await editor.click();
+
+        const actions = VSBrowser.instance.driver.actions();
+        await actions.move({ origin: focusTargetLement }).perform();
+        await utils.delay(5000);
+
+        const hoverContents = editor.findElement(By.className('hover-contents'));
+        const hoverValue = await hoverContents.getText();
+        console.log("Hover text:" + hoverValue);
+
+        assert(hoverValue.includes(constants.LOG_LEVEL_INFO_MSG), 'Did not get expected hover data for bootstrap.properties.');
+        editor.clearText();
+        await utils.closeEditor(constants.BOOTSTRAP_PROPERTIES);
+
+    }).timeout(45000);
+
+    it('Should show type ahead support in bootstrap.properties for a Liberty Server Configuration booststrap.properties entry', async () => {
+        await utils.openConfigFile(constants.CONFIG_TWO, constants.BOOTSTRAP_PROPERTIES);
+        editor = await new EditorView().openEditor(constants.BOOTSTRAP_PROPERTIES) as TextEditor;
+
+        await editor.typeTextAt(1, 1, constants.WS_LOGGING_T);
+        await utils.delay(5000);
+        //open the assistant
+        await utils.callAssitantAction(editor, constants.WS_LOG_TRACE_FORMAT);
+
+        await editor.typeTextAt(1, 32, constants.TBA);
+        await utils.delay(5000);
+
+        await utils.callAssitantAction(editor, constants.TBASIC);
+        await editor.toggleContentAssist(false);
+
+        const updatedBootstrapContent = await editor.getText();
+        await utils.delay(3000);
+        console.log("Content after type ahead support : ", updatedBootstrapContent);
+        assert(updatedBootstrapContent.includes(constants.WS_LOGLEVEL_TBASIC), 'Type ahead support is not worked as expected in bootstrap.properties');
+
+        editor.clearText();
+        await utils.closeEditor(constants.BOOTSTRAP_PROPERTIES);
+
+    }).timeout(45000);
+
+    after(() => {
+        utils.removeDirectoryByPath(path.join(utils.getGradleProjectPath(), 'src', 'main', 'liberty', 'config2'));
+        console.log("Removed new config folder:");
+    });
+
+});
--- src/test/definitions/constants.ts
+++ src/test/definitions/constants.ts
@@ -1,3 +1,4 @@
+
 /**
  * Copyright (c) 2020, 2022 IBM Corporation.
  *
@@ -9,7 +10,7 @@
  */
 
 export const MAVEN_PROJECT = "liberty.maven.test.wrapper.app";
-export const GRADLE_PROJECT= "liberty.gradle.test.wrapper.app";
+export const GRADLE_PROJECT = "liberty.gradle.test.wrapper.app";
 export const START_DASHBOARD_ACTION = "Start";
 export const STOP_DASHBOARD_ACTION = "Stop";
 export const START_DASHBOARD_MAC_ACTION = "Liberty: Start";
@@ -37,10 +38,68 @@ export const GRADLE_TEST_REPORT_TITLE = "liberty.gradle.test.wrapper.app test re
 export const ATTACH_DEBUGGER_DASHBOARD_ACTION = "Attach debugger";
 export const ATTACH_DEBUGGER_DASHBOARD_MAC_ACTION = "Liberty: Attach debugger";
 /** Maven: Dev mode debug port argument key. */
-export const  MAVEN_DEVMODE_DEBUG_PORT_PARM = "-DdebugPort";
+export const MAVEN_DEVMODE_DEBUG_PORT_PARM = "-DdebugPort";
 /** Gradle: Dev mode debug port argument key. */
 export const GRADLE_DEVMODE_DEBUG_PORT_PARM = "--libertyDebugPort";
 export const CONFIG = "config";
 export const CONFIG_TWO = "config2";
 export const BOOTSTRAP_PROPERTIES = "bootstrap.properties";
 export const SERVER_XML = "server.xml";
+export const SERVER_ENV = "server.env";
+export const CLOSE_EDITOR = "View: Close Editor";
+export const CONFIRM_MESSAGES = {
+  [SERVER_XML]: 'Do you want to save the changes you made to server.xml?',
+  [SERVER_ENV]: 'Do you want to save the changes you made to server.env?',
+  [BOOTSTRAP_PROPERTIES]: 'Do you want to save the changes you made to bootstrap.properties?'
+};
+export const EXPECTED_OUTCOME_WRONG = "'wrong' is not a valid value of union type 'booleanType'.";
+export const TARGETED_VALUE_LOGGING = '<logging appsWriteJson = \"wrong\" />';
+export const FOCUS_WRONG = "//*[contains(text(), 'wrong')]";
+export const SNIPPET_LOGGING = "<logging appsWriteJson = \"true\" />";
+export const FOCUS_QUICKFIX = "//*[contains(text(), 'Quick Fix')]";
+export const LOGGING_TRUE = "//*[contains(text(), \"Replace with 'true'\")]";
+export const FOCUS_HTTPENDPOINT = "//*[contains(text(), 'httpEndpoint')]";
+export const FOCUS_MPHEALTH = "//*[contains(text(), 'mpHealth')]";
+export const FEATURE_MPHEALTH = '<feature>mpHealth-4.0</feature>';
+export const NEWLINE = '\n';
+export const DESCRIPTION_MPHEALTH = "Description: This feature provides support for the MicroProfile Health specification.";
+export const DESCRIPTION_HTTPENDPOINT = `Configuration properties for an HTTP endpoint.`;
+export const FEATURE_EL = "<feature>el-3.0</feature>";
+export const LOGGING_TAG = "<logging></logging>";
+export const LOGGING = 'logging';
+export const FEATURE_TAG = 'feature';
+export const EL_VALUE = 'el-3.0';
+export const PLATFORM_JAKARTA_VALUE = "<platform>jakartaee-11.0</platform>";
+export const DESCRIPTION_PLATFORM = `Description: This platform resolves the Liberty features that support the Jakarta EE 11.0 platform.`;
+export const FOCUS_JAKARTA = "//*[contains(text(), '\jakarta\')]";
+export const PLATFORM_JAKARTA = "<platform>jakarta</platform>";
+export const PLATFORM_JAKARTA_ERROR = `ERROR: The platform "jakarta" does not exist.`;
+export const FOCUS_JAKARTA_ELEVEN = "//*[contains(text(), \"Replace platform with jakartaee-11.0\")]";
+export const FEATURE_SERVLET = "<feature>servlet</feature>";
+export const SERVLET_ERROR = `ERROR: The "servlet" versionless feature cannot be resolved since there are more than one common platform. Specify a platform or a feature with a version to enable resolution`;
+export const FOCUS_SERVLET = "//*[contains(text(), '\servlet\')]";
+export const SERVLET_VALUE = "<feature>servlet-3.1</feature>";
+export const FOCUS_SERVLET_VALUE = "//*[contains(text(), \"Replace feature with servlet-3.1\")]";
+export const PLATFORM = "platform";
+export const JAKARTA_ELEVEN = 'jakartaee-11.0';
+export const PLATFORM_JAKARTA_NINE = "<platform>jakartaee-9.1</platform>";
+export const WLP_LOGGING_CONSOLE_FORMAT = 'WLP_LOGGING_CONSOLE_FORMAT';
+export const CONSOLE_FORMAT_TBASIC = 'WLP_LOGGING_CONSOLE_FORMAT=TBASIC';
+export const TBASIC = 'TBASIC';
+export const TBA = "=TBA";
+export const WLP_LOGGING_CON = 'WLP_LOGGING_CON';
+export const LOG_LEVEL_INFO_MSG = 'This setting controls the granularity of messages that go to the console. The valid values are INFO, AUDIT, WARNING, ERROR, and OFF. The default is AUDIT. If using with the Eclipse developer tools this must be set to the default.';
+export const FOCUS_LOGLEVEL = "//*[contains(text(), 'CONSOLE_LOGLEVEL')]";
+export const LOGLEVEL_WITH_VALUE = 'WLP_LOGGING_CONSOLE_LOGLEVEL=OFF';
+export const CONSOLE_FORMAT_DIAGNOSTIC = 'The value `sample_value_is_updating_as_nodata` is not valid for the variable `WLP_LOGGING_CONSOLE_FORMAT`.';
+export const FOCUS_NODATA = "//*[contains(text(), 'nodata')]";
+export const VALUE_NODATA = '=sample_value_is_updating_as_nodata';
+export const WS_LOGGING_CON = "com.ibm.ws.logging.con";
+export const VALUE_WRONG = "=wrong";
+export const WS_LOGGING_CONSOLE_FORMAT = "com.ibm.ws.logging.console.format";
+export const WS_LOGGING_CONSOLE_DIAGNOSTIC = "The value `wrong` is not valid for the property `com.ibm.ws.logging.console.format`.";
+export const WS_LOGGING_CONSOLE_VALUE = "com.ibm.ws.logging.console.log.level=OFF";
+export const FOCUS_WS_LOGLEVEL = "//*[contains(text(), 'ws.logging.console.log.level')]";
+export const WS_LOGLEVEL_TBASIC = 'com.ibm.ws.logging.trace.format=TBASIC';
+export const WS_LOG_TRACE_FORMAT = 'com.ibm.ws.logging.trace.format';
+export const WS_LOGGING_T = "com.ibm.ws.logging.t";
