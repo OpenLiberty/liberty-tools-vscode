92147ef13be9bd422f098ebcc7181aa8586a0bf1
JoshwinThomasIBM <JoshwinThomas@ibm.com>
2025-02-07 20:06:17 +0530

    added comment

--- src/test/utils/testUtils.ts
+++ src/test/utils/testUtils.ts
@@ -192,8 +192,10 @@ export async function clearCommandPalette() {
   const buttons =  await dialog.getButtons();
   expect(buttons.length).equals(2);
   await dialog.pushButton('Clear');
-} 
-
+}
+/**
+ * Function clears the mvn plugin cache 
+ */
 export async function clearMavenPluginCache(): Promise<void> {
   // Check if the platform is Linux or macOS
   const homeDirectory = process.platform === 'linux' || process.platform === 'darwin' ? process.env.HOME // For Linux/macOS, use HOME
