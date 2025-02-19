36c18aca104a97db7459dec856bb690dced2ec1f
gilbysunil14 <Gilby.Sunil@ibm.com>
2025-02-19 10:05:32 +0530

    generic failure message

--- src/test/resources/ci/scripts/exec.sh
+++ src/test/resources/ci/scripts/exec.sh
@@ -97,7 +97,7 @@ main() {
 
     # If there were any errors, gather some debug data before exiting.
     if [ "$failure" = "true" ]; then
-        echo "ERROR: Failure occurred while running the tests."
+        echo "ERROR: Failure occurred while running ${TYPE} step."
 
         if [ $TYPE = "TEST" ]; then
             echo "DEBUG: Maven Liberty messages.log:\n"
