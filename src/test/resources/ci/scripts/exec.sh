9c700fec102ffb71ae6f388688e15efbd99100c7
gilbysunil14 <Gilby.Sunil@ibm.com>
2025-02-20 11:07:21 +0530

    updated copyright

--- src/test/resources/ci/scripts/exec.sh
+++ src/test/resources/ci/scripts/exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ############################################################################
-# Copyright (c) 2022 IBM Corporation and others.
+# Copyright (c) 2022, 2025 IBM Corporation and others.
 # 
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License v. 2.0 which is available at
