3b6666b41103d6617b74d0dc48adc0ff3db21fb6
Matt Bowersox <m.bowersox@ibm.com>
2026-01-14 14:43:31 -0600

    Reformat Gradle test files

--- src/test/GradleInitProject.ts
+++ src/test/GradleInitProject.ts
@@ -2,27 +2,18 @@
  * IBM Confidential
  * Copyright IBM Corp. 2023, 2025
  */
-import {  WebDriver, VSBrowser } from 'vscode-extension-tester';
+
+import { WebDriver, VSBrowser } from 'vscode-extension-tester';
 import * as utils from './utils/testUtils';
 
-
-
 describe('Open Gradle Project', () => {
-
-    let driver: WebDriver;  
-    
+    let driver: WebDriver;
 
     before(() => {
         driver = VSBrowser.instance.driver;
-        
     });
 
-    it('Open Sample Gradle Project', async () => {       
+    it('Open Sample Gradle Project', async () => {
         await VSBrowser.instance.openResources(utils.getGradleProjectPath());
-
     }).timeout(15000);
-
-    
 });
-
-
